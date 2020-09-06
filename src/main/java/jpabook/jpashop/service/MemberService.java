3ae1475dc0a385e507d0bb138785c6b783e6eac3
ROMANIAPEOPLE <39195377+ROMANIAPEOPLE@users.noreply.github.com>
2020-09-06 23:42:07 +0900

    회원 서비스 생성

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -2,26 +2,54 @@ package jpabook.jpashop.service;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class MemberService {
-    @Autowired
-    private MemberRepository memberRepository;
+
+    //@Autowired 필드인젝션 권장X
+    private final MemberRepository memberRepository;
+
+
+    //    @Autowired   세터인젝션 권장X
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
 
     //회원가입
+    @Transactional
     public Long join(Member member) {
 
         validateDuplicateMember(member);
-
         memberRepository.save(member);
         return member.getId();
 
     }
 
+    //중복처리
     private void validateDuplicateMember(Member member) {
         //exception
+        List<Member> findMembers =memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()) {
+            throw  new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+
+    //회원 전체 조회
+    public List<Member> findMember() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
     }
 
 }
