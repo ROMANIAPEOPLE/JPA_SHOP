1077597a732e4735db64402f2c0d29b55ca92024
ROMANIAPEOPLE <39195377+ROMANIAPEOPLE@users.noreply.github.com>
2020-09-12 00:50:15 +0900

    완성(복습 예정일 TODO)

---
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+}
+
+// TODO: 2020-09-12
+// JPA ORM 표준으로 Entity설계방법 완강 후 다시 보기.
